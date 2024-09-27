e738a4f8e5ee90cf3d75051ec8ae55ffbe487c64
TeeMan508 <t.shshudro@alumni.nsu.ru>
2024-09-27 17:50:21 +0300

    Main task api template

--- duplicates/backend/main.py
+++ duplicates/backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+from .task_api import check_video_duplicate, VideoLinkRequest
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/check-video-duplicate")
+async def task_api(request: VideoLinkRequest):
+    return check_video_duplicate(request)
+
---
+++ duplicates/backend/task_api/__init__.py
@@ -0,0 +1,4 @@
+from .video_link_request import VideoLinkRequest
+from .video_link_response import VideoLinkResponse
+from .check_video_duplicate import check_video_duplicate
+
