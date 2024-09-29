e6610e3cf1f44e26e96c05155da5d04bcd220c05
AliRN <ali_ramazanov_2000@mail.ru>
2024-09-29 03:37:47 +0300

    add ручки

--- duplicates/backend/web/api/v1/handler.py
+++ duplicates/backend/web/api/v1/handler.py
@@ -1,9 +1,10 @@
 import contextlib
+import tempfile
 from http.client import HTTPException
 from typing import Any
 
 import msgpack
-from fastapi import Depends
+from fastapi import Depends, UploadFile, File
 from pydantic import BaseModel
 from sqlalchemy import select, text
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -45,6 +46,20 @@ async def first_post_handler(body: tmp, session: AsyncSession = Depends(get_db),
     return ORJSONResponse({'message': 'perduk!'}, status_code=200)
 
 
+@router.get('/puk')
+async def first_handler(session: AsyncSession = Depends(get_db), ):
+    row = await session.execute(select(text('1')))
+    result, = row.one()
+
+    row1 = await session.scalars(select(text('1')))
+    result1 = row1.one()
+    logger.info('Testim %s; %s', result, result1)
+
+    await publish_message({'test': 'test'})
+
+    return ORJSONResponse({'message': 'perduk!'}, status_code=200)
+
+
 @router.post("/check-video-duplicate-front",
              response_model=videoLinkResponseFront,
              responses={
@@ -56,6 +71,114 @@ async def first_post_handler(body: tmp, session: AsyncSession = Depends(get_db),
 async def check_video_duplicate_front(video: videoRequestFront):
     try:
         has_duplicate, potential_duplicate_uuid = await search_duplicate(video.link)
+
+        if len(potential_duplicate_uuid) > 0:
+            duplicate_link = f"https://s3.ritm.media/yappy-db-duplicates/{potential_duplicate_uuid}.mp4"
+        else:
+            duplicate_link = potential_duplicate_uuid
+
+        # Публикация результата проверки в очередь сообщений
+        message_body = {
+            "event": "link_video",
+            "is_duplicate": has_duplicate,
+            "duplicate_link": duplicate_link
+        }
+        await publish_message(message_body)
+
+        if has_duplicate:
+            return videoLinkResponseFront(
+                is_duplicate=True,
+                link_duplicate=f"https://s3.ritm.media/yappy-db-duplicates/{potential_duplicate_uuid}.mp4",
+            )
+
+        return videoLinkResponseFront(
+            is_duplicate=False, link_duplicate="",
+        )
+
+
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail="Неверный запрос")
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail="Ошибка сервера")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Ошибка сервера")
+
+
+@router.post("/check-video-duplicate",
+             response_model=videoLinkResponse,
+             responses={
+                 400: {"description": "Неверный запрос"},
+                 500: {"description": "Ошибка сервера"}
+             },
+             tags=["API для проверки дубликатов видео"],
+             summary="Проверка видео на дублирование")
+async def check_video_duplicate(videoLink: videoLinkRequest):
+    try:
+        has_duplicate, potential_duplicate_uuid = await search_duplicate(videoLink.link)
+        print(has_duplicate, potential_duplicate_uuid)
+        message_body = {
+            "event": "video_duplicate_checked",
+            "is_duplicate": has_duplicate,
+            "duplicate_for": potential_duplicate_uuid
+        }
+        await publish_message(message_body)
+
+        if has_duplicate:
+            return videoLinkResponse(
+                is_duplicate=True, duplicate_for=potential_duplicate_uuid,
+            )
+
+        return videoLinkResponse(
+            is_duplicate=False, duplicate_for="",
+        )
+
+
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail="Неверный запрос")
+    except RuntimeError as e:
+        raise HTTPException(status_code=500, detail="Ошибка сервера")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Ошибка сервера")
+
+
+@router.post(
+    "/check-video-file-duplicate-front",
+    response_model=videoLinkResponseFront,
+    tags=["API для загрузки видео"],
+    summary="Загрузка видеофайла",
+    responses={
+        400: {"description": "Неверный запрос"},
+        500: {"description": "Ошибка сервера"}
+    }
+)
+async def upload_video(file: UploadFile = File(...), confidenceLevel: float = Form(...)):
+    print(file.content_type)  # Отладочное сообщение
+    if file.content_type != "video/mp4":
+        raise HTTPException(status_code=400, detail="Допускаются только файлы MP4.")
+
+    try:
+        video_bytes = await file.read()
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video:
+            temp_video.write(video_bytes)
+            temp_video.flush()
+            temp_video.close()
+
+        has_duplicate, potential_duplicate_uuid = await search_duplicate(temp_video.name)
+        print(has_duplicate, potential_duplicate_uuid)
+        if len(potential_duplicate_uuid) > 0:
+            duplicate_link = f"https://s3.ritm.media/yappy-db-duplicates/{potential_duplicate_uuid}.mp4"
+        else:
+            duplicate_link = potential_duplicate_uuid
+
+        message_body = {
+            "event": "upload_video",
+            "is_duplicate": has_duplicate,
+            "duplicate_for": potential_duplicate_uuid
+        }
+
+        await publish_message(message_body)
 
         if has_duplicate:
             return videoLinkResponseFront(
@@ -73,20 +196,6 @@ async def check_video_duplicate_front(video: videoRequestFront):
         raise HTTPException(status_code=500, detail="Ошибка сервера")
     except Exception as e:
         raise HTTPException(status_code=500, detail="Ошибка сервера")
-
-
-@router.get('/puk')
-async def first_handler(session: AsyncSession = Depends(get_db), ):
-    row = await session.execute(select(text('1')))
-    result, = row.one()
-
-    row1 = await session.scalars(select(text('1')))
-    result1 = row1.one()
-    logger.info('Testim %s; %s', result, result1)
-
-    await publish_message({'test': 'test'})
-
-    return ORJSONResponse({'message': 'perduk!'}, status_code=200)
 
 
 async def publish_message(body: dict[str, Any]) -> None:
